7544854a552060d6e1f9c38f3e60a99b2e4c035b
Adam Schwartz <adamschwartz@Adams-MacBook-Air.local>
2022-01-10 13:50:43 -0600

    update summary of suspension coil csv

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,4 +10,9 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 
 SuspensionCoil <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F) # import suspension coil csv
 
-head(SuspensionCoil)
+total_summary <- SuspensionCoil %>% summarize(Mean=mean(PSI),Median=median(PSI), Variance=var(PSI),SD=sd(PSI)) #Generate summary statistics
+
+view(total_summary)
+
+lot_summary <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI), Variance=var(PSI),SD=sd(PSI)) #Generate summary statistics by Lot
+                                             
