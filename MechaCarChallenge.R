b21bd4504e1468d19417ef002b21d6cd7996dbce
Adam Schwartz <adamschwartz@Adams-MacBook-Air.local>
2022-01-10 12:27:03 -0600

    add multiple linear regression code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,5 +1,9 @@
 library(tidyverse)
 
-MechaCar <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F) #import MechaCar_mpg dataset )
+MechaCar <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F) #import MechaCar_mpg dataset
 
 head(MechaCar)
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar) #generate multiple linear regression model
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar)) #generate summary statistics
